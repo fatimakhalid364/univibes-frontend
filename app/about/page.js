962767068906e4dfff9867694448ae0bf0911cf9
fatimakhalid364 <khalidfatima364@gmail.com>
2025-06-26 13:08:08 +0500

    about page in progress - process and mission pending

--- app/about/page.js
+++ app/about/page.js
@@ -1,12 +1,14 @@
 import { AboutHero } from "@/components/about/Hero";
 import { Quote } from "@/components/about/Quote";
 import { Team } from "@/components/about/Team";
+import { Process } from "@/components/about/Process";
 export default function About() {
     return (
         <>
             <AboutHero/>
             <Quote/>
             <Team/>
+            <Process/>
         </>
     );
 }
--- components/about/Process.jsx
+++ components/about/Process.jsx
@@ -2,7 +2,12 @@ import mainStyles from "@/styles/about/process.module.css"
 export const Process = () => {
     return (
         <section className={mainStyles.mainContainer}>
-            
+            <div className={mainStyles.headerContainer}>
+                <h2>OUR PROCESS</h2>
+            </div>
+            <div className={mainStyles.bodyContainer}>
+                
+            </div>
         </section>
     )
 }
