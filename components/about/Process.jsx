023d6bf541a0189b7c668d812bcfd9b0dcd24eda
fatimakhalid364 <khalidfatima364@gmail.com>
2025-06-26 14:22:49 +0500

    about page in progress - mission pending

--- components/about/Process.jsx
+++ components/about/Process.jsx
@@ -1,4 +1,5 @@
-import mainStyles from "@/styles/about/process.module.css"
+import mainStyles from "@/styles/about/process.module.css";
+import Image from "next/image";
 export const Process = () => {
     return (
         <section className={mainStyles.mainContainer}>
@@ -6,8 +7,61 @@ export const Process = () => {
                 <h2>OUR PROCESS</h2>
             </div>
             <div className={mainStyles.bodyContainer}>
-                
+                <div className={mainStyles.row1}>
+                    <div className={mainStyles.row1col1}>
+                        <div className={mainStyles.row1col1content}>
+                            <Image className={mainStyles.numImg} src="/pointer-1.svg" alt="pointer" width={40} height={120} />
+                            <div className={mainStyles.row1colTextContainer}>
+                                <h3 className={mainStyles.heading3}>TELL US ABOUT YOURSELF</h3>
+                                <p className={mainStyles.para}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={mainStyles.row2col2}>
+                        <div className={mainStyles.row2col2content}>
+                            <Image className={mainStyles.numImg} src="/pointer-2.svg" alt="pointer" width={40} height={120} />
+                            <div className={mainStyles.row1colTextContainer}>
+                                <h3 className={mainStyles.heading3}>TELL US ABOUT YOURSELF</h3>
+                                <p className={mainStyles.para}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={mainStyles.row1}>
+                    <div className={mainStyles.row1col1}>
+                        <div className={mainStyles.row1col1content}>
+                            <Image className={mainStyles.numImg} src="/pointer-3.svg" alt="pointer" width={40} height={120} />
+                            <div className={mainStyles.row1colTextContainer}>
+                                <h3 className={mainStyles.heading3}>TELL US ABOUT YOURSELF</h3>
+                                <p className={mainStyles.para}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={mainStyles.row2col2}>
+                        <div className={mainStyles.row2col2content}>
+                            <Image className={mainStyles.numImg} src="/pointer-4.svg" alt="pointer" width={40} height={120} />
+                            <div className={mainStyles.row1colTextContainer}>
+                                <h3 className={mainStyles.heading3}>TELL US ABOUT YOURSELF</h3>
+                                <p className={mainStyles.para}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <span className={mainStyles.shape1}>
+                <p className={mainStyles.tag}>COUNSELLING</p>
+            </span>
+            <span className={mainStyles.shape2}>
+                <p className={mainStyles.tag}>COUNSELLING</p>
+            </span>
         </section>
     )
 }
